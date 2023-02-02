fab9b306a937e2634040ee1319adb4235196d03a
yamina169 <111058141+yamina169@users.noreply.github.com>
2023-02-02 21:57:36 +0100

    Update OptionSelection.jsx

--- components/OptionSelection.jsx
+++ components/OptionSelection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export default function OptionSelection({ arrayItems, selectOption }) {
   return (
     <>
-      <h1 className="heading">ChatGPT Clone</h1>
+      <h1 className="heading">Yam ChatGPT</h1>
       <div className="grid-main">
         {arrayItems.map((item) => {
           return (
